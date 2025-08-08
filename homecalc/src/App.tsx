19e93dbfa6b2b821ce73f7643be90c9c732b463c
Archetypestan <vandeneeckhoutstan@gmail.com>
2025-08-08 13:20:10 +0200

    Topbar finished

--- homecalc/src/App.tsx
+++ homecalc/src/App.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="topbar">
+        <h1 className="logo">HomeCalc</h1>
+      </div>
+
+      <div className="main">
+        <div className="sidebar">
+          <ul>
+            <li>Dashboard</li>
+            <li>Instellingen</li>
+            <li>Over</li>
+          </ul>
+        </div>
+
+        <div className="content">
+          <p>Welkom bij HomeCalc 🎉</p>
+        </div>
+      </div>
     </div>
   );
 }
