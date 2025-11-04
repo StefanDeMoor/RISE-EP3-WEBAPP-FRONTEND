b4b8edd658958dc08687c2bf9abd73136aca4c54
StefanDeMoor <stefan.demoor@student.hogent.be>
2025-11-04 10:07:01 +0100

    We halen de overviews op + een aparte overviewpage + kiezen tussen overviewpage en emptymainsub

---
+++ homecalc/src/views/overview/OverviewPage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useOverviews } from "../../hooks/overview/useOverviews";
+import { OverviewPanel } from "../../components/overview/OverviewPanel";
+
+export const OverviewPage: React.FC = () => {
+  const { overviews, loading, error } = useOverviews();
+
+  if (loading) return <p>Loading overviews...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
+
+  if (overviews.length === 0) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <p>No overviews found in the database.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {overviews.map((o) => (
+        <OverviewPanel
+          key={o.id}
+          item={{ title: o.title, amount: o.totalIncome ?? 0 }}
+        />
+      ))}
+    </div>
+  );
+};
